e55a9ef22fa35d783e45c863be86f4c8da163936
Riley Richardson <103966138+rileyrich03@users.noreply.github.com>
2024-09-04 17:28:58 -0400

    Add files via upload

--- extension-test/popup.js
+++ extension-test/popup.js
@@ -1,24 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
-  let userInput = document.getElementById('userInput');
-  let button = document.getElementById('button');
-  let blacklist = [];
-  let blacklistUl = document.getElementById('blacklist');
-  let toggle = document.getElementById('toggle');
+let userInput = document.getElementById('userInput');
+let button = document.getElementById('button');
+let blacklist = [];
+let blacklistUl = document.getElementById('blacklist');
+let toggle = document.getElementById('toggle');
 
-  chrome.storage.local.get('blacklist', function(data) {
-    if (data.blacklist) {
-      blacklist = data.blacklist;
-    }
-  
-    blacklist.forEach(function(site) {
-      let li = document.createElement('li');
-  
-      li.textContent = site;
-      blacklistUl.appendChild(li);
-    });
+chrome.storage.local.get('blacklist', function(data) {
+  if (data.blacklist) {
+    blacklist = data.blacklist;
+  }
+
+  blacklist.forEach(function(site) {
+    if(site != null)
+		addWebsite(site);
   });
-  
-  //init toggle button
+});
+
+//init toggle button
 chrome.storage.local.get(["toggleState"], function(data) {
 	if (data.toggleState === 'On') {
 		toggle.innerText = 'On';
@@ -30,12 +28,6 @@ chrome.storage.local.get(["toggleState"], function(data) {
 button.addEventListener('click', function() {
   let site = userInput.value.trim();
   
-  try {
-    new URL(site); 
-  } catch (error) {
-    return; 
-  }
-  
   if (site) {
     chrome.storage.local.get('blacklist', function(data) {
       let blacklist = data.blacklist || [];
@@ -44,23 +36,15 @@ button.addEventListener('click', function() {
         blacklist.push(site);
         chrome.storage.local.set({ blacklist: blacklist }, function() {
        
-        let li = document.createElement('li');
-        li.textContent = site;
-        blacklistUl.appendChild(li);
+        addWebsite(site);
 
         userInput.value = '';
        });
       }
     });
    }
-  });
 
-  userInput.addEventListener('keypress', function(event) {
-    if (event.key === "Enter") {
-      button.click();
-    }
   });
-
   //Toggle button for extension
   toggle.addEventListener('click', function() {
 	chrome.storage.local.get(['toggleState'], function(data) {
@@ -72,4 +56,22 @@ button.addEventListener('click', function() {
 		})
 	});
   });
+  //function used when adding websites to the blacklist
+  function addWebsite (site) {
+	let li = document.createElement('li');
+
+    li.textContent = site;
+	
+	blacklistUl.appendChild(li);
+
+	let deleteButton = document.createElement("button");
+	deleteButton.innerText = "delete";
+	deleteButton.addEventListener("click", function() {
+		delete blacklist[blacklist.indexOf(site)];
+		chrome.storage.local.set({blacklist: blacklist}, function(){});
+		li.remove();
+	})
+	li.appendChild(deleteButton);
+  }
 });
+
