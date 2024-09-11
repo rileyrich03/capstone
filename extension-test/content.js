0fc551441ee6f999df5ab23d0f9e0d7c5fdf7da7
JosephGCurtin <123776093+JosephGCurtin@users.noreply.github.com>
2024-09-11 15:29:31 -0400

    Added first injection to content.js, tabs now interact with extension when blacklisted

--- extension-test/content.js
+++ extension-test/content.js
@@ -1,1 +1,29 @@
+chrome.storage.local.get(['blacklist', 'toggleState'], function(data) {
+    if (data.blacklist && data.toggleState === 'On')
+        {
+        let currentURL = (window.location.href.replace(/\/+$/, ""));
 
+        data.blacklist.forEach(function(site)
+        {
+            let blacklistedURL = site.replace(/\/+$/, "");
+
+            if (currentURL.startsWith(blacklistedURL)) {
+
+                let hugeBlueDiv = document.createElement('div');
+
+                document.body.style.color = "blue";
+                document.body.style.backgroundColor = "blue";
+                hugeBlueDiv.style.backgroundColor = "blue";
+
+                hugeBlueDiv.style.width = "100%";
+                hugeBlueDiv.style.height = "100%";
+
+                hugeBlueDiv.style.position = "fixed";
+                hugeBlueDiv.style.top = "0";
+                hugeBlueDiv.style.left = "0";
+
+                document.body.appendChild(hugeBlueDiv);
+            }
+        });
+    }
+});
