9e2dc4072635a9873ace55f89b7399f3f5c69049
JosephGCurtin <123776093+JosephGCurtin@users.noreply.github.com>
2024-09-15 13:37:39 -0400

    Update content.js to change users cursor into an image

--- extension-test/content.js
+++ extension-test/content.js
@@ -1,29 +1,59 @@
-chrome.storage.local.get(['blacklist', 'toggleState'], function(data) {
-    if (data.blacklist && data.toggleState === 'On')
-        {
-        let currentURL = (window.location.href.replace(/\/+$/, ""));
+function warningCursor() {
+  chrome.storage.local.get(['blacklist', 'toggleState'], function(data) {
+    if (data.blacklist && data.toggleState === 'On') 
+    {
 
-        data.blacklist.forEach(function(site)
-        {
-            let blacklistedURL = site.replace(/\/+$/, "");
+      const isBlacklisted = data.blacklist.some(function(site) {
+  
+        try {
+          const blacklistedURL = new URL(site).hostname;
+          if (window.location.hostname === blacklistedURL) {
+            return true;
+          }
+          else {
+            return false;
+          }          
+        } 
+        catch (error) {
+          return false;
+        }
+      
+      });
+      
+      if (isBlacklisted == true) {
 
-            if (currentURL.startsWith(blacklistedURL)) {
+        document.documentElement.style.cursor = 
+        `url('${chrome.runtime.getURL('cursor.png')}'), default`;
+      }
 
-                let hugeBlueDiv = document.createElement('div');
+/*       
+let currentURL = (window.location.href.replace(/\/+$/, ""));
 
-                document.body.style.color = "blue";
-                document.body.style.backgroundColor = "blue";
-                hugeBlueDiv.style.backgroundColor = "blue";
+      data.blacklist.forEach(function(site)
+      {
+          let blacklistedURL = site.replace(/\/+$/, "");
 
-                hugeBlueDiv.style.width = "100%";
-                hugeBlueDiv.style.height = "100%";
+          if (currentURL.startsWith(blacklistedURL)) {
 
-                hugeBlueDiv.style.position = "fixed";
-                hugeBlueDiv.style.top = "0";
-                hugeBlueDiv.style.left = "0";
+              let hugeBlueDiv = document.createElement('div');
 
-                document.body.appendChild(hugeBlueDiv);
-            }
-        });
+              document.body.style.color = "blue";
+              document.body.style.backgroundColor = "blue";
+              hugeBlueDiv.style.backgroundColor = "blue";
+
+              hugeBlueDiv.style.width = "100%";
+              hugeBlueDiv.style.height = "100%";
+
+              hugeBlueDiv.style.position = "fixed";
+              hugeBlueDiv.style.top = "0";
+              hugeBlueDiv.style.left = "0";
+
+              document.body.appendChild(hugeBlueDiv);
+          } 
+  */
+
     }
-});
+  });
+}
+
+warningCursor();
